e842b18367931aa7938d9e0bc934ad35d32d20c4
Timothy <lemayiant4@gmail.com>
2023-06-17 18:09:43 +0300

    done on product page

--- pages/singlepage/index.jsx
+++ pages/singlepage/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles.module.css'
 import Image from 'next/image'
 import Header from "../../components/surfaces/Header"
 import Footer from "../../components/surfaces/Footer"
 import Carousel from '../../components/surfaces/Carousel'
 import pdisplay from "../../public/pdisplay.jpeg";
+import uparrow from "../../public/uparrow.svg"
+import downarrow from '../../public/downarrow.svg'
 
 function singlepage() {
     const images = [
@@ -12,6 +14,7 @@ function singlepage() {
         pdisplay,
         pdisplay
     ]
+    const [additionalFeatures, setAdditionalFeatures] = useState(false)
     return (
         <div className={styles.fbreijhj}>
             <Header />
@@ -86,13 +89,47 @@ function singlepage() {
             </div>
 
             <div className={styles.lgonly}>
-            <h3 className={styles.oewfprrf}>3 bedroom Bungalow</h3>
                 <div className={styles.firkej}>
                     <div className={styles.frwvrio}>
+                        <h3 className={styles.oewfprrf}>3 bedroom Bungalow</h3>
                         <div className={styles.mainimage}>
                             <Image src={pdisplay} className={styles.singlehouse} alt='house3' ></Image>
                         </div>
+                        <div className={styles.addfcontainer}>
+                        <div className={styles.showsaddfeatures}>
+
+                            <div className={styles.jcorsdo}>
+                                <h4 className={styles.jrpeks}>ADDITIONAL FEATURES</h4>
+                                {additionalFeatures ?
+                                    <div onClick={() => setAdditionalFeatures(false)} >
+                                        <Image src={uparrow} alt='up' className={styles.downsvg} ></Image>
+                                    </div>
+                                    :
+                                    <div onClick={() => setAdditionalFeatures(true)}>
+                                        <Image src={downarrow} alt='up' className={styles.downsvg}></Image>
+                                    </div>
+                                }
+                            </div>
+                            {additionalFeatures ?
+                                <div className={styles.sliderdown}>
+                                    <hr ></hr>
+                                    <div className={styles.hfiuero}>
+                                        <ul>
+                                            <li className={styles.vcbreu}>Heated Floors</li>
+                                            <li className={styles.vcbreu}>High Ceiling</li>
+                                            <li className={styles.vcbreu}>Hot Shower</li>
+                                            <li className={styles.vcbreu}>Pressurized Water</li>
+                                            <li className={styles.vcbreu}>Garbage Dispenser</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                :
+                                null
+                            }
+                        </div>
+                        </div>
                         <div className={styles.vbebjrf}>
+                        {/* <hr className={styles.singlehr} ></hr> */}
                             <h3 className={styles.nverios}>Reply to this listing</h3>
                             <form className={styles.nvervre}>
                                 <input className={styles.wefhiwf} type='text' placeholder='Name' ></input>
@@ -104,7 +141,7 @@ function singlepage() {
                     </div>
                     <div className={styles.fvuheri}>
                         <div className={styles.singlesdescription}>
-                            <h3 className={styles.singleitemprice}>Ksh 30 000 000</h3>
+                            {/* <h3 className={styles.singleitemprice}>Ksh 30 000 000</h3>
                             <div className={styles.descriptionholder}>
                                 <div className={styles.fhurie}>
                                     <p className={styles.pidshc}>Property Type:</p>
@@ -142,22 +179,27 @@ function singlepage() {
                                     <p className={styles.pidshc}>Bedrooms:</p>
                                     <p className={styles.chwi}> 3</p>
                                 </div>
-                            </div>
+                            </div> */}
                             <div>
                                 <div className={styles.singlefeatures}>
                                     <h3 className={styles.cbwefe}>Features</h3>
                                     <ul className={styles.lkdir}>
-                                        <li className={styles.vcbreu}>Heated Floors</li>
-                                        <li className={styles.vcbreu}>High Ceiling</li>
-                                        <li className={styles.vcbreu}>Hot Shower</li>
-                                        <li className={styles.vcbreu}>Pressurized Water</li>
-                                        <li className={styles.vcbreu}>Garbage Dispenser</li>
+                                        <li className={styles.vcbreu}>Residential Houses</li>
+                                        <li className={styles.vcbreu}>2 Years construction period</li>
+                                        <li className={styles.vcbreu}>Kerugoya, Kirinyaga</li>
+                                        <li className={styles.vcbreu}>Binding Contract</li>
+                                        <li className={styles.vcbreu}>2 Bathrooms</li>
+                                        <li className={styles.vcbreu}>3 Bedrooms</li>
                                     </ul>
+                                    <h4 className={styles.prncjre}>PLAN PRICE</h4>
+                                    <h3 className={styles.sdjvreu}>Ksh 30 000 000</h3>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
+
             </div>
             <Footer />
         </div>
